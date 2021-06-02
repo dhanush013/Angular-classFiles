39976eedac9b629250f492df589fdc24f8fe270d
dhanush013 <40061153+dhanush013@users.noreply.github.com>
2021-06-02 15:48:39 +0530

    ..

--- Project1/src/app/product/product.component.ts
+++ Project1/src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { prd_data } from './productData';
 
 @Component({
   selector: 'app-product',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  prd1:Product=new Product(9001,'Apple','Chennai');
+  prd:Product[];
+  
+
+  constructor() {
+    this.prd = prd_data;
+  }
 
   ngOnInit(): void {
   }
---
+++ Project1/src/app/product/productData.ts
@@ -0,0 +1,8 @@
+export const prd_data=[
+    {prdId:8001,prdName:"pen" ,prdLocation:"pune" },
+    {prdId:8002,prdName:"pencil" ,prdLocation:"chennai" },
+    {prdId:8003,prdName:"shirt" ,prdLocation:"delhi" },
+    {prdId:8004,prdName:"pant" ,prdLocation:"kolkata" },
+    {prdId:8005,prdName:"trousers" ,prdLocation:"mumbai" },
+    {prdId:8006,prdName:"carrot" ,prdLocation:"ooty" }
+]
